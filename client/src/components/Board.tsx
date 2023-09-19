f8490ccd528629f6d95f61276d69be4f7a316c42
TrulyBright <trulybright@yonsei.ac.kr>
2023-09-19 19:09:13 +0800

    shorten river

--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -1,5 +1,4 @@
 import {Cell, CellProps} from './Cell'
-import { River } from './River'
 import styles from "./Board.module.css"
 
 // 10 rows, 9 columns
@@ -11,13 +10,9 @@ export const Board: React.FC<BoardProps> = (board) => {
     const flattened = board.cells.flatMap((row, i) => {
         return row.map((cell, j) => (<Cell cell={cell.cell} key={`${i}, ${j}`}></Cell>))
     })
-    const upper = flattened.slice(0, 45)
-    const lower = flattened.slice(45)
-    const rivers = Array(9).fill(null).map((_, j) => (<River key={`River ${j}`}></River>))
-    const cells = upper.concat(rivers).concat(lower)
     return (
         <div className={styles.board}>
-            {cells}
+            {flattened}
         </div>
     )
 }
--- client/src/components/River.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "./Board.module.css"
-
-export const River: React.FC = () => {
-    return (
-        <div className={`${styles.river} ${styles.cell}`}></div>
-    )
-}
